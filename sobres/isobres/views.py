34378bfbb8f34745d428cd4bd15b2fbe249817ac
ncoso77 <coso77@gmail.com>
2014-03-27 18:21:20 +0100

    Update views.py

--- sobres/isobres/views.py
+++ sobres/isobres/views.py
@@ -13,6 +13,40 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.context_processors import csrf
+
+from forms import SignUpForm
+ 
+ 
+def signup(request):
+    if request.method == 'POST':  # If the form has been submitted...
+        form = SignUpForm(request.POST)  # A form bound to the POST data
+        if form.is_valid():  # All validation rules pass
+ 
+            # Process the data in form.cleaned_data
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            email = form.cleaned_data["email"]
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+ 
+            # At this point, user is a User object that has already been saved
+            # to the database. You can continue to change its attributes
+            # if you want to change other fields.
+            user = User.objects.create_user(username, email, password)
+            user.first_name = first_name
+            user.last_name = last_name
+ 
+            # Save new user attributes
+            user.save()
+ 
+            return HttpResponseRedirect(/)  # Redirect after POST
+    else:
+        form = SignUpForm()
+ 
+    data = {
+        'form': form,
+    }
+    return render_to_response('signup.html', data, context_instance=RequestContext(request))
     
 def mainpage(request):
 	template = get_template('mainpage.html')
