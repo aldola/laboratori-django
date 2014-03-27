70b989c05547bb97408d0ebafba6e0a3ec09c2da
ncoso77 <coso77@gmail.com>
2014-03-27 12:53:22 +0100

    Update urls.py

--- sobres/sobres/urls.py
+++ sobres/sobres/urls.py
@@ -28,7 +28,7 @@ urlpatterns = patterns('',
     url(r'^hostals/(\w+)/$', hostal),
     url(r'^hostals', hostals),
     url(r'^login/$','django.contrib.auth.views.login'), 
-    url(r'^usuarinou/$','principal.views.nou_usuario'),
+    url(r'^usuarinou/$','principal.views.nou_usuari'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 )
